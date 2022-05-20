4aff576295ebae804fbc93b4ce181e1a4e504047
Elitsa Marinova <elimarinova123@gmail.com>
2022-05-20 11:14:13 +0200

    Add project files.

---
+++ Chat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public void btn_OnClick(object? sender, RoutedEventArgs args)
+        {
+            var button = (sender as Button)!;
+            switch (button.Name)
+            {
+                case "btn":
+                    {
+                        button.Content = "Ginger";
+                    }
+                    break;
+                case "otherBtn":
+                    {
+                        button.Content = "Ale";
+                    }
+                    break;
+                default:
+                    {
+                        button.Content = "No clue which Button you are!";
+                    }
+                    break;
+            }
+        }
+    }
+}
